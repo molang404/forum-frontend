1ebf9f1e41c89d4a5d129be22b498d574f9a6ee2
molang404 <art104233@naver.com>
2026-05-12 17:14:18 +0900

    MainLayout / Theme 팔레트

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,32 @@
+import { Outlet } from "react-router";
+import styled from "styled-components";
+import MainHeader from "../components/layout/MainHeader.tsx";
+import MainFooter from "../components/layout/MainFooter.tsx";
+
+const LayoutWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100dvh;
+`;
+
+const MainContainer = styled.main`
+    flex: 1;
+    width: 100%;
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 40px 20px;
+`;
+
+function MainLayout() {
+    return (
+        <LayoutWrapper>
+            <MainHeader />
+            <MainContainer>
+                <Outlet />
+            </MainContainer>
+            <MainFooter />
+        </LayoutWrapper>
+    );
+}
+
+export default MainLayout;
--- src/router/GetRouter.tsx
+++ src/router/GetRouter.tsx
@@ -2,9 +2,12 @@ import { createBrowserRouter } from "react-router";
 import HomePage from "../pages/HomePage.tsx";
 import SignInPage from "../pages/auth/signin/SignInPage.tsx";
 import SignUpPage from "../pages/auth/signup/SignUpPage.tsx";
+import MainLayout from "../layouts/MainLayout.tsx";
 
 const router = createBrowserRouter([
-    { path: "/", children: [
+    { path: "/",
+        element: <MainLayout />,
+        children: [
             { index: true, element: <HomePage /> },
             { path: "auth", children: [
                     { path: "signin", element: <SignInPage /> },
